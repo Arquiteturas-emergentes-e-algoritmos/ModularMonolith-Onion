cf3c28b44b1db86c059c3c8a0e0526dff4a605c9
Kauã Vinícius <kauaponte@hotmail.com>
2024-10-28 18:41:08 -0300

    Medication plan onion

---
+++ MedicationPlan/Domain/Medication.cs
@@ -0,0 +1,17 @@
+﻿using Common.Domain;
+
+namespace MedicationPlan.Domain;
+
+public class Medication : Entity
+{
+    public Medication() { }
+
+    public Medication(string name, DateTime takeAt)
+    {
+        Name = name;
+        TakeAt = takeAt;
+    }
+
+    public string Name { get; set; } = string.Empty;
+    public DateTime TakeAt { get; set; } = DateTime.MaxValue;
+}
---
+++ MedicationPlan/Domain/MedicationPlan.cs
@@ -0,0 +1,26 @@
+﻿using Common.Domain;
+
+namespace MedicationPlan.Domain;
+public class MedicationPlan : Entity
+{
+    public List<Medication> Medications { get; set; } = [];
+    public DateTime BeginAt { get; set; } = DateTime.UtcNow;
+    public DateTime FinishAt { get; set; } = DateTime.MaxValue;
+
+    public void AddMedication(Medication m)
+    {
+        Medications.Add(m);
+        _ = Medications.OrderBy(m => m.TakeAt.TimeOfDay - DateTime.UtcNow.TimeOfDay);
+    }
+
+    public void RemoveMedication(Guid Id) => Medications.RemoveAll(m => m.Id == Id);
+
+    public void UpdateMedication(Medication m)
+    {
+        var medicationFound = Medications.Find(x => x.Id == m.Id);
+        if (medicationFound == null) return;
+        Medications.Remove(medicationFound);
+        Medications.Add(m);
+        _ = Medications.OrderBy(t => t.TakeAt.TimeOfDay);
+    }
+}
