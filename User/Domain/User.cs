cf5b69c718dfb5052eb124cbaa0f386f258cc2af
Kauã Vinícius <kauaponte@hotmail.com>
2024-10-28 18:41:16 -0300

    User Onion

---
+++ User/Domain/User.cs
@@ -0,0 +1,12 @@
+﻿using Common.Domain;
+
+namespace User.Domain;
+
+public class User : Entity, IObserver
+{
+    public Guid MedicationPlanId { get; set; } = Guid.Empty;
+    public Guid GlucometerId { get; set; } = Guid.Empty;
+    public void Update()
+    {
+    }
+}
