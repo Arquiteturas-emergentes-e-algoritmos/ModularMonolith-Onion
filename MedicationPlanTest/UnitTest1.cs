db993712092da91b1df1c1c206b4ab6c99d6ddb3
Kauã Vinícius <kauaponte@hotmail.com>
2024-11-08 22:41:46 -0300

    Init tests

---
+++ MedicationPlanTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace MedicationPlanTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ UserTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace UserTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
